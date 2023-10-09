104d5bc42df56eda76c87cfc8a0bac26be769aa4
Aidos <b.aidos02@mail.ru>
2023-10-09 12:44:29 +0600

    Add HW-1

---
+++ Lesson_1/src/HW_1.java
@@ -0,0 +1,14 @@
+public class HW_1 {
+    public static void main(String[] args) {
+        System.out.println("Привет, мир! Я изучаю Java.");
+        //один
+
+        /*
+
+          много
+
+        */
+
+        //TODO: Yep
+    }
+}
--- Lesson_1/src/Main.java
+++ Lesson_1/src/Main.java
@@ -2,9 +2,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!" + 5 + 6 + "\n");
-        System.out.print(100);
-        //TODO: Yep
 
     }
 }
